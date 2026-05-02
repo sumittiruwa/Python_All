b7be513f66a0c9f1897f549744d8e03df947f68f
khushimainali <sumittiruwa913@gmail.com>
2026-05-02 21:25:50 +0545

    index and slicing

--- String/string.py
+++
@@ -1,4 +0,0 @@
-str1 = "this is a string"
-str2 = 'hello world'
-print(str1)
-print(str2)
---
+++ string and Conditional Statement/string.py
@@ -0,0 +1,28 @@
+str1 = "this is a string"
+
+# \ this is a tab for space
+# \n for next line
+str2 = 'hello.\n world'
+print(str1)
+print(str2)
+
+
+# concANTATION
+
+str3 = 'hello'
+str4 = "world"
+print(str3 + " " + str4)
+
+
+
+# lenght
+# -- len also count the space 
+
+print(len(str1))
+
+
+# Indexing and slicing
+str5 = "python programming"
+print(str5[5])
+ch = str5[6]
+print(ch)
