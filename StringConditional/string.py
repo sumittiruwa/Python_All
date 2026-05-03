0e6a8fc86ee87731050646bf122de4801262c6b4
khushimainali <sumittiruwa913@gmail.com>
2026-05-03 18:07:52 +0545

    slicing

---
+++ StringConditional/string.py
@@ -0,0 +1,40 @@
+str1 = "this is a string"
+
+# \ this is a tab for space
+# \n for next line
+str2 = 'hello.\n world'
+print(str1)
+print(str2)
+
+
+# concANTATION
+
+str3 = 'hello'
+str4 = "world"
+print(str3 + " " + str4)
+
+
+
+# lenght
+# -- len also count the space 
+
+print(len(str1))
+
+
+# Indexing and slicing
+str5 = "python programming"
+print(str5[5])
+ch = str5[6]
+print(ch)
+
+
+# Slicing
+# -- it is used to get a part of the string
+# -- it is done by using the index of the string
+
+str6 = "hello world"
+print(str6[0:4]) # it will print hell
+print(str6[6:11]) # it will print world
+
+
+
