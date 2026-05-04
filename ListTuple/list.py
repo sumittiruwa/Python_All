d46b7b77524f1539447c33a0c3811479a3e5b193
khushimainali <sumittiruwa913@gmail.com>
2026-05-04 22:44:53 +0545

    list and tuple

---
+++ ListTuple/list.py
@@ -0,0 +1,16 @@
+# data type that store set  of values
+
+
+makrs = [90, 80, 70, 60, 50]
+print(makrs)
+print(makrs[0]) # it will print 90
+print(makrs[1]) # it will print 80          
+print(makrs[2]) # it will print 70
+print(makrs[3]) # it will print 60
+
+# list is mutable
+makrs[0] = 95   
+
+print(makrs) # it will print [95, 80, 70, 60, 50]
+
+print(type(makrs)) # it will print <class 'list'>
