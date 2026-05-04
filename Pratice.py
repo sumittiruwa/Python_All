ce2cf1785793d0391c038a764cdeaf7cb2687db8
khushimainali <sumittiruwa913@gmail.com>
2026-05-04 22:27:46 +0545

    practie the questions

---
+++ Pratice.py
@@ -0,0 +1,10 @@
+
+# name and count the words in the name
+input("Enter your name:")
+name = input("Enter your name:")
+print("Length of the name is " + str(len(name)))
+
+
+#
+str1 = "this is a string"
+
