7c3d48813d158134d1b20183270a9c6ff0d69dec
khushimainali <sumittiruwa913@gmail.com>
2026-05-07 21:32:08 +0545

    tuplies learn

---
+++ ListTuple/Tuples.py
@@ -0,0 +1,11 @@
+# its is built in data structure in python
+# it is ordered collection of items  . immutable (can't change the value of tuple) and allows duplicate values
+
+tuple1 = (1, 2, 3, 4, 5)
+print(tuple1) # it will print (1, 2, 3, 4
+tuple1[0] = 10 # it will give error because tuple is immutable
+print(tuple1) # it will print (1, 2, 3, 4
+
+tup = ()
+print(type(tup)) # it will print <class 'tuple'>
+print(len(tup)) # it will print 0
