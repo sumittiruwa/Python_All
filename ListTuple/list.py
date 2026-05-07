134cc4d32873dcb299f28d6293982562836d92cf
khushimainali <sumittiruwa913@gmail.com>
2026-05-07 20:56:10 +0545

    Mutable and Immutable

--- ListTuple/list.py
+++ ListTuple/list.py
@@ -20,3 +20,17 @@ print(len(makrs)) # it will print 5
 #you can store different data type in a list
 my_list = [1, "hello", 3.14, True]
 print(my_list)
+
+
+# immutable  - string  (can't change the value of string)
+#muttable - list (can change the value of list)
+
+
+student = ("mitsu" , 23, "bhatktapur")
+print(student)
+print(student[0]) # it will print mitsu
+print(student[1]) # it will print 23
+print(student[2]) # it will print bhatktapur
+
+student[0] = "ram" # it will give error because tuple is immutable  
+print(student)
