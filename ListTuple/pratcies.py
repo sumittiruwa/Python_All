8f8155026ca51f7cd4fd3e95317b5505fe1ea048
khushimainali <sumittiruwa913@gmail.com>
2026-05-07 21:32:45 +0545

    pratcie the questions

---
+++ ListTuple/pratcies.py
@@ -0,0 +1,12 @@
+# program to the user enter name of its favourite movie and store them in a list 
+
+movies = []
+mov1 = input("Enter the name of your favourite movie:")
+mov2 = input("Enter the name of your favourite movie:")
+mov3 = input("Enter the name of your favourite movie:")
+
+
+movies.append(mov1)
+movies.append(mov2)     
+movies.append(mov3)
+print(movies) # it will print the list of movies entered by the user
