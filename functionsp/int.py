4880b88a8de37d999fc1bc8a09bb4c0f1964d737
khushimainali <sumittiruwa913@gmail.com>
2026-05-08 23:07:46 +0545

    updated

--- functionsp/int.py
+++ functionsp/int.py
@@ -1,5 +1,11 @@
-def cal_sum(a, b):
-    sum = a + b
-    print(f"The sum of {a} and {b} is: {sum}")      
-    return a + b
+# def cal_sum(a, b):
+#     sum = a + b
+#     print(f"The sum of {a} and {b} is: {sum}")      
+#     return a + b
 
+
+
+def greet():
+    print("Hello World")
+
+greet()
