db51c1c1b1eb5853989a590ef1545219890d87e4
khushimainali <sumittiruwa913@gmail.com>
2026-05-09 08:56:42 +0545

    questions pratice

---
+++ functionsp/questions.py
@@ -0,0 +1,16 @@
+#program to print the length of the list
+cities = ["kathmandu", "bhaktapur", "lalitpur"]
+heroes = ["naruto", "sasuke", "sakura"]
+
+def print_length(lst):
+    print(len(lst))
+print_length(cities)
+
+print_length(cities)
+print_length(heroes)
+
+
+
+print(heroes[0], end ="\n") # it will print naruto
+print(heroes[1], end ="\n") # it will print sasuke
+print(heroes[2]) # it will print sakura
