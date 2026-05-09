752982ac2cef6bf42156cde5fcebc01ee0087cb4
khushimainali <sumittiruwa913@gmail.com>
2026-05-09 09:00:10 +0545

    fatcorioal learn

--- functionsp/questions.py
+++ functionsp/questions.py
@@ -14,3 +14,13 @@ print_length(heroes)
 print(heroes[0], end ="\n") # it will print naruto
 print(heroes[1], end ="\n") # it will print sasuke
 print(heroes[2]) # it will print sakura
+
+
+
+# program to find the factorial of a number
+
+n = 5
+fact = 1
+for i in range(1, n+1):
+    fact *= i
+print(f"The factorial of {n} is: {fact}")
