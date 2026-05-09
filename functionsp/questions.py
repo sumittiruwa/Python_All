58317fb09b75c77116f3b4ab6d536f5d7026266c
khushimainali <sumittiruwa913@gmail.com>
2026-05-09 09:02:13 +0545

    by function fact

--- functionsp/questions.py
+++ functionsp/questions.py
@@ -24,3 +24,12 @@ fact = 1
 for i in range(1, n+1):
     fact *= i
 print(f"The factorial of {n} is: {fact}")
+
+
+# by function
+def factorial(n):
+    fact = 1
+    for i in range(1, 1+n):
+        fact *= i
+        return fact
+    print(factorial(5))
