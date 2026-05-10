fb3b73cfc4973aa4255dceea72f9d7737c3936db
khushimainali <sumittiruwa913@gmail.com>
2026-05-10 09:42:05 +0545

    updtded

---
+++ Python_in_class/Infocard.py
@@ -0,0 +1,9 @@
+list = ["sbodh", "20", "29.09.2003", True]
+print(list) # it will print the list
+print(type(list)) # it will print the type of the variable
+print(list[1]) # it will print the first element of the list
+
+
+x , y, z, a = list
+
+print(z)
