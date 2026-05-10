23043d39cbb333a89709977a4e9f3deaf73486cf
khushimainali <sumittiruwa913@gmail.com>
2026-05-10 09:40:16 +0545

    celsius and fahrentinne pratcie

---
+++ Python_in_class/classtwo.py
@@ -0,0 +1,11 @@
+#temperature Cnverter 
+
+celsius = float(input("Enter the temperature in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"The temperature in Fahrenheit is: {fahrenheit}")
+kelvin = celsius + 273.15
+print(f"The temperature in Kelvin is: {kelvin}")
+print(type(celsius)) # it will print <class 'float'>
+print(type(fahrenheit)) # it will print <class 'float'>
+print(type(kelvin)) # it will print <class 'float'>
+
