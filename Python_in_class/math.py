4b95e4f51c2b34afc131bc2488026bac53da6582
khushimainali <sumittiruwa913@gmail.com>
2026-05-13 19:37:09 +0545

    area  and perimeter

---
+++ Python_in_class/math.py
@@ -0,0 +1,9 @@
+length = 110
+breadth = 50
+area = length * breadth
+perimeter = 2 * (length + breadth)
+print("Area of the rectangle is:", area)
+print("Perimeter of the rectangle is:", perimeter)  
+print(type(length)) # it will print <class 'int'>
+print(type(breadth)) # it will print <class 'int'>
+print(type(area)) # it will print <class 'int'> 
