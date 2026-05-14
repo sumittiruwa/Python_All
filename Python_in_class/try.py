c45df5326d14063e6d3be6c0e6423cd3bccc4c09
khushimainali <sumittiruwa913@gmail.com>
2026-05-14 21:25:51 +0545

    ipdated

---
+++ Python_in_class/try.py
@@ -0,0 +1,6 @@
+status = "single"
+if status == "single": 
+    print("I am single")
+elif status == "married":
+    print("I am married")
+else:    print("I am not sure about my relationship status")
