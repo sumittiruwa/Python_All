071e368d085defdbb6cd36dc6026f9a7ccaafd04
khushimainali <sumittiruwa913@gmail.com>
2026-05-15 08:43:47 +0545

    Typecasting

--- Python_in_class/Typecast/intro.py
+++ Python_in_class/Typecast/intro.py
@@ -1,5 +1,17 @@
 # typecasting and conditional
 
+#input function always returns a string, so we need to typecast it to the desired data type.
+
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
- print(f"Hello {name}, you are {age} years old.")
+print(f"Hello {name}, you are {age} years old.")
+
+#typecasting is the process of converting one data type to another. It can be done using built-in functions like int(), float(), str(), etc.
+
+int_value = 10
+float_value = float(int_value)  
+double_value = float_value * 2
+print(f"Integer value: {int_value}, Float value: {float_value}, Double value: {double_value}")
+
+
+
