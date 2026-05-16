19788d6305a0afadbb8fcc49f2f5cbfb54ad3fea
khushimainali <sumittiruwa913@gmail.com>
2026-05-16 15:36:48 +0545

    updated

---
+++ Python_in_class/Week_one/Typecast/challenges.py
@@ -0,0 +1,5 @@
+# temperature converter
+celcius = input("Enter the : ")
+
+frahranheit = (frahranheit * 9/5) + 32
+print(f"The temperature in Fahrenheit is: {frahranheit}")
---
+++ Python_in_class/Week_one/Typecast/smart.py
@@ -0,0 +1,12 @@
+name = input("Enter your name: ")
+age = input("Enter your age:")
+eligible = input("Are you eligible to vote? (yes/no): ")
+
+if age>=18:
+    print("elegible to voote")
+else:
+    print("not eligible to vote")
+
+
+
+    
