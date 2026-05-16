2912143e7a163071b97c0686e372cddcce163bb7
khushimainali <sumittiruwa913@gmail.com>
2026-05-16 14:06:37 +0545

    ML Model reco

--- Python_in_class/AssigmentFirstWeek/Qtwo.py
+++ Python_in_class/AssigmentFirstWeek/Qtwo.py
@@ -12,14 +12,13 @@ model2 = float(input("Enter the accuracy of Model 2: "))
 model3 = float(input("Enter the accuracy of Model 3: "))
 
 if model1 > model2 and model1 > model3:
-    best_model = "Model 1"
+    print("The best model is: Model 1")
 elif model2 > model1 and model2 > model3:
-    best_model = "Model 2"
+    print("The best model is: Model 2")
 else:
-    best_model = "Model 3"
+    print("The best model is: Model 3")
 
 all_exceed_90 = model1 > 90 and model2 > 90 and model3 > 90
 any_below_70 = model1 < 70 or model2 < 70 or model3 < 70
-print(f"The best model is: {best_model}")
 if all_exceed_90:
     print("All models exceed 90% accuracy. Great performance!") 
