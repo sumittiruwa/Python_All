d5d9f6e598a08a41574fda6f1fa81323651b66c3
khushimainali <sumittiruwa913@gmail.com>
2026-05-16 16:49:12 +0545

    login form

---
+++ Pratice/Login.py
@@ -0,0 +1,10 @@
+username = "mistu"
+password = "sumit"
+
+name = input("Enter your username: ")
+passw = input("Enter your password: ")
+
+if name == username and passw == password:
+    print("Login successful!")
+else:
+    print("Invalid username or password")
