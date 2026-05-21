6d369454673703ae1b2f92d4190d41c6307572bf
khushimainali <sumittiruwa913@gmail.com>
2026-05-21 22:11:46 +0545

    inheritance

---
+++ oops/in.py
@@ -0,0 +1,25 @@
+# OOPs Example in Python
+
+class Student:
+
+    # Constructor
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # Method
+    def display(self):
+        print("Name:", self.name)
+        print("Age:", self.age)
+
+
+# Creating objects
+s1 = Student("Sumit", 20)
+s2 = Student("Ram", 21)
+
+# Calling methods
+s1.display()
+
+print()
+
+s2.display()
