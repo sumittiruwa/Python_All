2a87f91dbb44e9b4c80d97d60e7167a67ae5c7b8
khushimainali <sumittiruwa913@gmail.com>
2026-05-25 12:42:33 +0545

    encapsu

--- oops/encapsulation/index.py
+++ oops/encapsulation/index.py
@@ -1,10 +1,14 @@
-#access modifier
-#private - are those method which is only accesble within a class 
-#constructor
+# Access Modifier
+# Private members are accessible only within the class
+
 class Person:
-    def __init__(self,name,age):
-        self.__name=name
-        self.__age=age
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
 
     def display_info(self):
-        print(f"the person name is {self.__name}and the age is  {self.__age}")
+        print(f"The person name is {self.__name} and the age is {self.__age}")
+
+
+p1 = Person("Sumit", 20)
+p1.display_info()
