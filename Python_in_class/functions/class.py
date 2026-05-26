02c2b654a2f3a850f025cf82342d0865e856c5e5
khushimainali <sumittiruwa913@gmail.com>
2026-05-26 09:20:23 +0545

    tyoes of argument

--- Python_in_class/functions/class.py
+++ Python_in_class/functions/class.py
@@ -10,3 +10,39 @@ def calculated_area(length, width):
 
 result = calculated_area(8, 5)
 print(f"{result}")
+
+
+# Types of function arguments
+
+#1 . positional
+
+def add(a,b):
+    return a+b
+print(add(3,5))
+
+# default
+
+def greet(name, msg="hi"):
+    print(f"{msg}, {name}!")
+    
+    greet("krishna ji ")
+    greet("subodh", "bye")
+    
+# keyword 
+
+def info(name, age, city):
+    print(f"{name}, {age}, {city}")
+    
+    info(age = 25, city = "kathmandu", name = "krihsna")
+    
+    
+#args / ** kwargs
+
+
+
+def total(*nums):
+    return sum(nums)
+
+def show(**info):
+    for k,v in info.items():
+        print(f"{k}:{v}")
