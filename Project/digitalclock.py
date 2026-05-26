6f29af15b7fc618d3d188c9484b9aa811d8c9d88
khushimainali <sumittiruwa913@gmail.com>
2026-05-26 17:22:25 +0545

    digital clock

--- Project/digitalclock.py
+++ Project/digitalclock.py
@@ -7,3 +7,12 @@ root.title("Digital Clock")
 
 def time():
     string  = strftime('%H:%M%S %p \n %d')
+    label.config(text=string)
+    label.after(1000, time)
+    
+label = tk.Label(root, font=('calibri', 50, 'bold'), background='yellow', foreground='black')
+label.pack(anchor='center')
+
+time()
+
+root.mainloop()
