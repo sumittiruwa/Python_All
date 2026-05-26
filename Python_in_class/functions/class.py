8e62d0261b46f83856ff404ae5a63a2dead3c83c
khushimainali <sumittiruwa913@gmail.com>
2026-05-26 09:12:41 +0545

    calculatror

--- Python_in_class/functions/class.py
+++ Python_in_class/functions/class.py
@@ -1,2 +1,12 @@
 # def function_nmae(param1, param2):
 #     the function return result
+
+
+def calculated_area(length, width):
+    area = length * width
+    return area
+
+#calling the function 
+
+result = calculated_area(8, 5)
+print(f"{result}")
