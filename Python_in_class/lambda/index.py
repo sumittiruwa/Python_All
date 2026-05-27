ac8a5c3bf3ec8e045b380fceccc56f55ed25b693
khushimainali <sumittiruwa913@gmail.com>
2026-05-27 08:55:44 +0545

    lambda

---
+++ Python_in_class/lambda/index.py
@@ -0,0 +1,11 @@
+# lambada 
+
+def square(x):
+    sq = lambda x:
+print(sq(5))
+        
+#lambda with osrted () - real ai use
+
+students = [("Krihsna", 78),("subohd", 09), ("beta", 89)]
+ranked = sorted(students, key = lambda s: s[1], reverse= True)
+print(ranked)
