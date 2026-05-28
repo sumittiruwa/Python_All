fedc6d7bf38b54b3d05783ed663e05b6f6d865d6
khushimainali <sumittiruwa913@gmail.com>
2026-05-28 11:48:38 +0545

    excel, json and csv file

--- pandas/save.py
+++ pandas/save.py
@@ -1,10 +1,9 @@
 import pandas as pd
 
 data = {
-    "Name":['Mnish','Krishna','bhupen',],
-    "Age":[10,20,30],
-    "city":['ktm','bhakpatpur', 'lalitpur']
-    
+    "Name": ['Mnish', 'Krishna', 'bhupen'],
+    "Age": [10, 20, 30],
+    "city": ['ktm', 'bhakpatpur', 'lalitpur']
 }
 
 df = pd.DataFrame(data)
@@ -12,4 +11,5 @@ df = pd.DataFrame(data)
 print(df)
 
 df.to_csv("output.csv", index=False)
- 
+df.to_excel("output.xlsx", index=False)
+df.to_json("output.json")
