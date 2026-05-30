462a4c2261b919d41bd67c75bcfcb8e242d284cb
Sumit <sumittiruwa913@gmail.com>
2026-05-31 01:09:03 +0545

    groupded

---
+++ pandas/Sorting/group.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+data = {
+    "Name":['Krishna','Sachin','Rahul','Subodh','Bhupen','Sarita','Sushant'],
+    "Age":[10,90,30,12,39,32,89],
+    "Salary":[2000,80000,20000,30000,90000,20000,80000]
+}
+
+df = pd.DataFrame(data)
+
+ 
+grouped = df.groupby("Age")["Salary"].sum()
+print(grouped)
