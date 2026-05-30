ee99c7170438a345c25d840797314498eafc40ec
Sumit <sumittiruwa913@gmail.com>
2026-05-31 00:42:34 +0545

    interpolaited

---
+++ pandas/Handeling_missing_data/inter.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+data = {
+    "Name":['Sumit','krihsna','subodh','sachin','bhupen', 'rahul','sarita','sushant','raxz'],
+    "Age":[20,90,20,12,34,67,19,56,45],
+    "Salary":[19000,20000,20000,3000,12000,90000,60000,12097,12793],
+    "Performance Score":[99,23,12,34,56,78,90,98,76]
+    
+}
+
+df = pd.DataFrame(data)
+print(df)
+"""
+method types-
+1.linear
+2.polynomial
+3.time
+"""
+
+df.interpolate(method="linear", axis=0, inplace=True)
