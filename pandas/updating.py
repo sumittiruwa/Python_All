db2f56847c2f6fe2a42b5a5a4ab49d4c59d516fc
khushimainali <sumittiruwa913@gmail.com>
2026-05-30 09:20:50 +0545

    updating multiple value

---
+++ pandas/updating.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+data = {
+    "Name":['Sumit','krihsna','subodh','sachin','bhupen', 'rahul','sarita','sushant','raxz'],
+    "Age":[20,29,20,12,34,67,19,56,45],
+    "Salary":[19000,20000,20000,3000,12000,90000,60000,12097,12793],
+    "Performance Score":[99,23,12,34,56,78,90,98,76]
+    
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+
+# increasing salary by  5 percent
+
+df['Salary'] = df['Salary'] * 1.05
+print(df)
