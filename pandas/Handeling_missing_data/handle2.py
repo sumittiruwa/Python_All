4e5942b86b5d919506c9f1b93163f76c38d92f47
khushimainali <sumittiruwa913@gmail.com>
2026-05-30 21:36:11 +0545

    handling

---
+++ pandas/Handeling_missing_data/handle2.py
@@ -0,0 +1,22 @@
+
+import pandas as pd
+
+data = {
+    "Name":['Sumit','krihsna','subodh','sachin','bhupen', 'rahul','sarita','sushant','raxz'],
+    "Age":[20,None,20,12,34,67,19,56,45],
+    "Salary":[19000,20000,20000,3000,12000,90000,60000,12097,12793],
+    "Performance Score":[99,23,12,34,56,78,90,98,76]
+    
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+
+df.dropna(inplace=True)
+print(df)
+
+df.fillna(0, inplace=True)
+print(df)
+
+
