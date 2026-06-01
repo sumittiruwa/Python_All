76f5627d100ac4090ca7356fe649450c4accfa9e
Sumit <sumittiruwa913@gmail.com>
2026-06-01 23:56:56 +0545

    updated

--- Python_in_class/Loops/Assigment/qseven.py
+++ Python_in_class/Loops/Assigment/qseven.py
@@ -1,4 +1,21 @@
-# Recursive function to calculate total images
+# . Recursive Dataset Folder Scanner 
+# A dataset is organized in nested folders: 
+# dataset = { 
+#    "train": { 
+#        "cats": 500, 
+#        "dogs": 450 
+#    }, 
+#    "test": { 
+#        "cats": 120, 
+#        "dogs": 130 
+#    } 
+# } 
+# Write a recursive function that traverses the nested dictionary and calculates the total number of 
+# images. 
+# Requirements: 
+# ● Use recursion. 
+# ● Function must work for any nesting level. 
+# ● Return the total image count. 
 def total_images(data):
     total = 0
 
