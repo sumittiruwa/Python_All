f3498fb974d3f3458a6f8fdb23972a046a806ee2
Sumit <sumittiruwa913@gmail.com>
2026-06-02 00:06:29 +0545

    last

---
+++ Python_in_class/Loops/Assigment/last.py
@@ -0,0 +1,14 @@
+# 10. Student Performance Ranking System 
+# Write a Python program that processes marks of multiple students. 
+# Requirements: 
+# ● Create a function that accepts a list of marks. 
+# ● Use loops to calculate grades: 
+# ○ A → 80 and above 
+# ○ B → 60–79 
+# ○ C → 40–59 
+# ○ F → Below 40 
+# ● Use a lambda function to sort marks in descending order. 
+# ● Display: 
+# ○ Sorted marks 
+# ○ Grade distribution 
+# ○ Number of passed students
