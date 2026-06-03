59c0d0b723a5d80146b8d0b723e45509308b9acf
Sumit <sumittiruwa913@gmail.com>
2026-06-03 21:16:14 +0545

    learning

---
+++ Python_in_class/string/str.py
@@ -0,0 +1,15 @@
+txt = "Krishna Chaudhary"
+
+print(len(txt))			# length of string
+
+print(ord(txt[0]))		# change to ASCII value
+
+print(max(txt.strip()))			# highest char
+
+print(sorted(txt.strip()))		# sorting char in string
+
+print(chr(75))			# find value of ASCII
+
+print(min(txt.strip()))	# lowest char
+
+print(''.join(reversed(txt)))
