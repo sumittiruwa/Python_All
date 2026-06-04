b04d68691007bad3f9f8ecc66ccde091ecacd9e6
Sumit <sumittiruwa913@gmail.com>
2026-06-05 01:33:13 +0545

    array in numpy

---
+++ Numpy/numarrayproperties/shape.py
@@ -0,0 +1,9 @@
+# arraye shape in numpy
+# when working with multi dimensional arrays, the shape of the array is an important aspect to consider. The shape of an array is a tuple that describes the dimensions of the array. It indicates how many elements are present in each dimension of the array.
+
+
+import numpy as np
+
+arr_2d = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(arr_2d.shape) # (3,3)
+
