5103b6b5679d78424cd843dc825513de666c39de
Sumit <sumittiruwa913@gmail.com>
2026-06-04 23:15:21 +0545

    # ceratibg sequences of numbers in numpy

---
+++ Numpy/creation/five.py
@@ -0,0 +1,11 @@
+# ceratibg sequences of numbers in numpy
+
+
+import numpy as np
+
+# arange(start, stop, step) function is used to create an array of evenly spaced values within a specified range
+# start: The starting value of the sequence (inclusive).
+# stop: The end value of the sequence (exclusive).
+# step: The spacing between values.
+arange_array = np.arange(0, 10, 2)
+print(arange_array)
