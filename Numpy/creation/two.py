5e32993785fb2003b760e0ae88fb5039af7a5265
Sumit <sumittiruwa913@gmail.com>
2026-06-04 23:08:54 +0545

    array with zeroes

---
+++ Numpy/creation/two.py
@@ -0,0 +1,8 @@
+# with deault value
+# np.zerors() function is used to create an array of zeros with a specified shape and data type
+
+
+import numpy as np
+
+zeros_array = np.zeros((3,4), dtype=np.int32)
+print(zeros_array)
