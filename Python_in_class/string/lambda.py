91122d1bb2e31e8fe1cd3fcbf72293f0285c50b8
Sumit <sumittiruwa913@gmail.com>
2026-06-04 08:01:31 +0545

    lambda with string

---
+++ Python_in_class/string/lambda.py
@@ -0,0 +1,20 @@
+# lambada for string transformation
+upper = lambda s: s.upper()
+print(upper("kingkong"))
+
+#sorting  string by length
+
+words = ["python", "java", "c++", "ruby", "javascript"]
+words.sort(key=lambda w: len(w))
+print(words)
+
+#using map() with lambda 
+
+names = ["alice", "bob", "charlie", "dave"]
+titled = list(map(lambda n: n.title(), names))
+print(titled)
+
+
+# filter: words longer than 4 cahr
+long_words = list(filter(lambda w: len(w) > 4, words))
+print(long_words)
