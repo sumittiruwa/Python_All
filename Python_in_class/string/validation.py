3abd3a4a0ef04a54e826fbc9d485f88b2c76a741
Sumit <sumittiruwa913@gmail.com>
2026-06-04 07:51:43 +0545

    validation in string

---
+++ Python_in_class/string/validation.py
@@ -0,0 +1,16 @@
+# is_() methods return true or false 
+
+txt = "Krishna Chaudhary"
+
+
+print(txt.isalpha())		# all char are alphabet
+print(txt.isdigit())		# all char are digit
+print(txt.isalnum())		# all char are alphabet or digit
+print(txt.islower())		# all char are in lower case
+print(txt.isupper())		# all char are in upper case
+print(txt.isspace())		# all char are space
+print(txt.istitle())		# all char are in title case
+print(txt.isprintable())	# all char are printable
+print(txt.isidentifier())	# all char are valid identifier\
+print(txt.startswith("krihsna"))	# string start with "Krishna"
+print(txt.endswith("chaudhary"))	# string end with "Chaudhary
