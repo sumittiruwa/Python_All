e8ad7df6988473f8ea9806ffbf62a2e399285123
Sumit <sumittiruwa913@gmail.com>
2026-06-04 23:10:16 +0545

    arrays with ones

---
+++ Numpy/creation/three.py
@@ -0,0 +1,6 @@
+# ones(shape, dtype=float, order='C') function is used to create an array of ones with a specified shape and data type
+
+import numpy as np
+
+ones_array = np.ones((2,3), dtype=np.float64)
+print(ones_array)
