40979be0b8cdd75f31a084b1fa2281c0730ad2d8
Sumit <sumittiruwa913@gmail.com>
2026-06-08 12:37:38 +0545

    fancy indexing

---
+++ Numpy/indexing/fancy.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+print(arr[0, 1])  # Output: 2
+print(arr[1, :])  # Output: [4 5 6]
+print(arr[[1,2,3], [0,1,2]])  # Output: [4 5 6]
+print(arr[[0,1,2], [2,1,0]])  # Output
