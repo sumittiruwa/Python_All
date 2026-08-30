88fcd60ea9110747e28ed526f70d8a317ce65951
Sumit <sumittiruwa913@gmail.com>
2026-08-30 19:20:06 +0545

    Add DSA practice: edit distance (levenshtein)

---
+++ python_ds/48_edit_distance.py
@@ -0,0 +1,25 @@
+"""DSA Practice: Edit Distance / Levenshtein Distance (Dynamic Programming)"""
+
+
+def edit_distance(word1, word2):
+    m, n = len(word1), len(word2)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+    for i in range(m + 1):
+        dp[i][0] = i
+    for j in range(n + 1):
+        dp[0][j] = j
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if word1[i - 1] == word2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+            else:
+                dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
+
+    return dp[m][n]
+
+
+if __name__ == "__main__":
+    print("Edit distance 'horse' -> 'ros':", edit_distance("horse", "ros"))
+    print("Edit distance 'intention' -> 'execution':", edit_distance("intention", "execution"))
