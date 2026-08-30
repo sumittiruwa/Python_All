4ec5446e84196d24ed80dba16d2750c310399d8f
Sumit <sumittiruwa913@gmail.com>
2026-08-30 19:20:05 +0545

    Add DSA practice: 0/1 knapsack problem

---
+++ python_ds/45_knapsack_problem.py
@@ -0,0 +1,23 @@
+"""DSA Practice: 0/1 Knapsack Problem (Dynamic Programming)"""
+
+
+def knapsack(weights, values, capacity):
+    n = len(weights)
+    dp = [[0] * (capacity + 1) for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        for w in range(capacity + 1):
+            if weights[i - 1] <= w:
+                dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w])
+            else:
+                dp[i][w] = dp[i - 1][w]
+
+    return dp[n][capacity]
+
+
+if __name__ == "__main__":
+    weights = [1, 3, 4, 5]
+    values = [1, 4, 5, 7]
+    capacity = 7
+
+    print("Maximum value in knapsack:", knapsack(weights, values, capacity))
